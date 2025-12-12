eb6572cd11ddf956e53e34c749ba0f78a3002091
fath-nasrudin <fath.nasrudin.i@gmail.com>
2025-12-12 17:06:04 +0700

    Update course service to use prisma instead of raw sql

--- src/features/course/course.service.ts
+++ src/features/course/course.service.ts
@@ -5,6 +5,7 @@ import {
   CreateCourseInputSchema,
   UpdateCourseInputSchema,
 } from "./course.schema";
+import { prisma } from "../../lib/prisma";
 
 let courses: Course[] = [
   {
@@ -37,43 +38,75 @@ let courses: Course[] = [
 interface CourseDB extends Course, RowDataPacket {}
 
 export async function getCourses(): Promise<Course[]> {
-  const [rows] = await db.query<CourseDB[]>("SELECT * from courses");
-  return rows;
+  // const [rows] = await db.query<CourseDB[]>("SELECT * from courses");
+  // return rows;
+  let courses = await prisma.courses.findMany();
+
+  return courses;
 }
 
 export async function createCourse(data: CreateCourseInputSchema) {
-  const [rows] = await db.execute(
-    "INSERT INTO courses (title, description) VALUES (?, ?)",
-    [data.title, data.description ? data.description : null]
-  );
-  return rows;
+  // const [rows] = await db.execute(
+  //   "INSERT INTO courses (title, description) VALUES (?, ?)",
+  //   [data.title, data.description ? data.description : null]
+  // );
+  // return rows;
+  return prisma.courses.create({
+    data: {
+      title: data.title,
+      description: data.description,
+    },
+  });
 }
 
 export async function getCourseById(id: Course["id"]): Promise<Course> {
-  const [[course]] = await db.execute<CourseDB[]>(
-    `SELECT 
-     c.id, c.title, c.description, 
-      JSON_ARRAYAGG(
-        JSON_OBJECT(
-          'id', cat.id, 
-          'name', cat.name
-          )
-      ) as categories 
-    FROM courses c
-    LEFT JOIN courses_categories cc on cc.course_id = c.id
-    LEFT JOIN categories cat ON cat.id = cc.category_id
-    WHERE c.id = ? 
-    GROUP BY c.id;
-    `,
-    [id]
-  );
+  // const [[course]] = await db.execute<CourseDB[]>(
+  //   `SELECT
+  //    c.id, c.title, c.description,
+  //     JSON_ARRAYAGG(
+  //       JSON_OBJECT(
+  //         'id', cat.id,
+  //         'name', cat.name
+  //         )
+  //     ) as categories
+  //   FROM courses c
+  //   LEFT JOIN courses_categories cc on cc.course_id = c.id
+  //   LEFT JOIN categories cat ON cat.id = cc.category_id
+  //   WHERE c.id = ?
+  //   GROUP BY c.id;
+  //   `,
+  //   [id]
+  // );
+  const dbCourse = await prisma.courses.findUnique({
+    where: { id },
+    include: {
+      courses_categories: {
+        omit: { category_id: true, course_id: true },
+        include: {
+          categories: {
+            select: {
+              id: true,
+              name: true,
+            },
+          },
+        },
+      },
+    },
+  });
+
+  if (!dbCourse) {
+    throw new Error(`Course with id: "${id}" not found`);
+  }
+
+  const course: Course = {
+    description: dbCourse.description,
+    id: dbCourse.id,
+    title: dbCourse.title,
+    category: dbCourse?.courses_categories.map((cc) => cc.categories),
+  };
 
   // @todo should fetch categories for the course
   // const thisCourseCategories =
-
-  if (!course) {
-    throw new Error(`Course with id: "${id}" not found`);
-  }
 
   return course;
 }
@@ -82,25 +115,31 @@ export async function updateCourseById(
   id: Course["id"],
   data: UpdateCourseInputSchema
 ) {
-  const fields: string[] = [];
-  const values = [];
+  // const fields: string[] = [];
+  // const values = [];
 
-  Object.entries(data).forEach(([key, value]) => {
-    fields.push(`${key} = ?`);
-    values.push(value);
-  });
+  // Object.entries(data).forEach(([key, value]) => {
+  //   fields.push(`${key} = ?`);
+  //   values.push(value);
+  // });
 
-  if (fields.length === 0) return Promise.resolve();
+  // if (fields.length === 0) return Promise.resolve();
 
-  values.push(id);
-  const [rows] = await db.execute(
-    `UPDATE courses SET ${fields.join(",")}  WHERE id = ?`,
-    values
+  // values.push(id);
+  // const [rows] = await db.execute(
+  //   `UPDATE courses SET ${fields.join(",")}  WHERE id = ?`,
+  //   values
+  // );
+  // return rows;
+  const filteredData = Object.fromEntries(
+    Object.entries(data).filter(([_, v]) => v !== undefined)
   );
-  return rows;
+
+  return prisma.courses.update({ where: { id }, data: filteredData });
 }
 
 export async function deleteCourseById(id: Course["id"]) {
-  const [rows] = await db.execute("DELETE FROM courses WHERE id = ?", [id]);
-  return rows;
+  // const [rows] = await db.execute("DELETE FROM courses WHERE id = ?", [id]);
+  // return rows;
+  return prisma.courses.delete({ where: { id } });
 }
