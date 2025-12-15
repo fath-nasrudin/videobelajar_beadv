861f924ede996de0cf3e434ef86fe0fe989a1f8c
fath-nasrudin <fath.nasrudin.i@gmail.com>
2025-12-15 19:57:58 +0700

    Add resend

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,4 +3,7 @@ export const config = {
     secret: process.env.EMAIL_VERIFICATION_SECRET as string,
     duration: 15 * 60 * 1000, // 15 minutes
   },
+  resend: {
+    apiKey: process.env.RESEND_API_KEY as string,
+  },
 };
